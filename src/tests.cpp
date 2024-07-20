31b0d22e2c2ad2dce5849951d49edbf8a8eea4c9
Elijah Kin <elijah.m.kin@gmail.com>
2024-07-20 15:14:47 -0700

    Refactoring tests

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,16 +1,5 @@
 #include "optimizer.h"
 
-void add_to_multiply_test() {
-  Instruction *x = CreateParameter();
-
-  Instruction *add = CreateBinary(kAdd, x, x);
-  Instruction *neg = CreateUnary(kNegate, add);
-
-  Optimizer opt;
-  opt.Optimize(add);
-  assert(neg->operand(0)->opcode() == kMultiply);
-}
-
 void factor_add_multiply_test() {
   Instruction *x = CreateParameter();
   Instruction *y = CreateParameter();
@@ -19,30 +8,94 @@ void factor_add_multiply_test() {
   Instruction *lhs = CreateBinary(kMultiply, x, y);
   Instruction *rhs = CreateBinary(kMultiply, x, z);
   Instruction *add = CreateBinary(kAdd, lhs, rhs);
-  Instruction *exp = CreateUnary(kExp, add);
 
   Optimizer opt;
   opt.Optimize(add);
-  assert(exp->operand(0)->opcode() == kMultiply);
+  assert(add->opcode() == kMultiply && add->operand(1)->opcode() == kAdd);
+}
+
+void add_to_multiply_test() {
+  Instruction *x = CreateParameter();
+
+  Instruction *add = CreateBinary(kAdd, x, x);
+
+  Optimizer opt;
+  opt.Optimize(add);
+  assert(add->opcode() == kMultiply);
+}
+
+void exp_log_test() {
+  Instruction *x = CreateParameter();
+  Instruction *y = CreateParameter();
+
+  Instruction *log_x = CreateUnary(kLog, x);
+  Instruction *exp_log_x = CreateUnary(kExp, log_x);
+  Instruction *exp_y = CreateUnary(kExp, y);
+  Instruction *log_exp_y = CreateUnary(kLog, exp_y);
+  Instruction *add = CreateBinary(kAdd, exp_log_x, log_exp_y);
+
+  Optimizer opt;
+  opt.Optimize(add);
+  assert(add->operand(0)->opcode() == kParameter &&
+         add->operand(1)->opcode() == kParameter);
+}
+
+void multiply_exp_to_exp_add_test() {
+  Instruction *x = CreateParameter();
+  Instruction *y = CreateParameter();
+
+  Instruction *exp_x = CreateUnary(kExp, x);
+  Instruction *exp_y = CreateUnary(kExp, y);
+  Instruction *mul = CreateBinary(kMultiply, exp_x, exp_y);
+
+  Optimizer opt;
+  opt.Optimize(mul);
+  assert(mul->opcode() == kExp && mul->operand(0)->opcode() == kAdd);
+}
+
+void double_negation_test() {
+  Instruction *x = CreateParameter();
+
+  Instruction *neg_x = CreateUnary(kNegate, x);
+  Instruction *neg_neg_x = CreateUnary(kNegate, neg_x);
+
+  Optimizer opt;
+  opt.Optimize(neg_neg_x);
+  assert(neg_neg_x->opcode() == kParameter);
 }
 
 void negate_subtract_folding_test() {
   Instruction *x = CreateParameter();
   Instruction *y = CreateParameter();
-  Instruction *z = CreateParameter();
 
   Instruction *sub = CreateBinary(kSubtract, x, y);
   Instruction *neg = CreateUnary(kNegate, sub);
-  Instruction *add = CreateBinary(kAdd, neg, z);
 
   Optimizer opt;
   opt.Optimize(neg);
-  assert(add->operand(0)->opcode() == kSubtract);
+  assert(neg->opcode() == kSubtract);
+}
+
+void subtract_logs_to_log_divide_test() {
+  Instruction *x = CreateParameter();
+  Instruction *y = CreateParameter();
+
+  Instruction *log_x = CreateUnary(kLog, x);
+  Instruction *log_y = CreateUnary(kLog, y);
+  Instruction *sub = CreateBinary(kSubtract, log_x, log_y);
+
+  Optimizer opt;
+  opt.Optimize(sub);
+  assert(sub->opcode() == kLog && sub->operand(0)->opcode() == kDivide);
 }
 
 int main() {
-  add_to_multiply_test();
   factor_add_multiply_test();
+  add_to_multiply_test();
+  exp_log_test();
+  multiply_exp_to_exp_add_test();
+  double_negation_test();
   negate_subtract_folding_test();
+  subtract_logs_to_log_divide_test();
   return 0;
 }
